8e6569c902299a9ad783d5039a53684aaad934f2
Matej Jellus <juffalow@juffalow.com>
2016-04-25 13:29:38 +0200

    problem 8 solved - int -> int64 :-(

--- archive/8/solution.go
+++ archive/8/solution.go
@@ -8,7 +8,6 @@ import (
 /*
 
 The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
-
 73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
@@ -29,113 +28,44 @@ The four adjacent digits in the 1000-digit number that have the greatest product
 84580156166097919133875499200524063689912560717606
 05886116467109405077541002256983155200055935729725
 71636269561882670428252483600823257530420752963450
-
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
-
 
 https://projecteuler.net/problem=8
 
 */
 
 func main() {
-    var number []string
+    var number string
     var adjacentDigits int
     
-    number = make([]string, 20)
+    number = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+    adjacentDigits = 13
     
-    number[0] = "73167176531330624919225119674426574742355349194934"
-    number[1] = "96983520312774506326239578318016984801869478851843"
-    number[2] = "85861560789112949495459501737958331952853208805511"
-    number[3] = "12540698747158523863050715693290963295227443043557"
-    number[4] = "66896648950445244523161731856403098711121722383113"
-    number[5] = "62229893423380308135336276614282806444486645238749"
-    number[6] = "30358907296290491560440772390713810515859307960866"
-    number[7] = "70172427121883998797908792274921901699720888093776"
-    number[8] = "65727333001053367881220235421809751254540594752243"
-    number[9] = "52584907711670556013604839586446706324415722155397"
-    number[10] = "53697817977846174064955149290862569321978468622482"
-    number[11] = "83972241375657056057490261407972968652414535100474"
-    number[12] = "82166370484403199890008895243450658541227588666881"
-    number[13] = "16427171479924442928230863465674813919123162824586"
-    number[14] = "17866458359124566529476545682848912883142607690042"
-    number[15] = "24219022671055626321111109370544217506941658960408"
-    number[16] = "07198403850962455444362981230987879927244284909188"
-    number[17] = "84580156166097919133875499200524063689912560717606"
-    number[18] = "05886116467109405077541002256983155200055935729725"
-    number[19] = "71636269561882670428252483600823257530420752963450"
+    var i int
+    var max int64
+    var product int64
 
-    adjacentDigits = 4
+    max = 0
     
-    var max int
-
-    max = findGreatestProductInRow(number, adjacentDigits)
-    fmt.Sprintf("The maximum product in line is : %v", max)
-
-    fmt.Println(max)
-    
-    max = findGreatestProductInCol(number, adjacentDigits)
+    for i = 0; i < 1000 - adjacentDigits; i++ {
+        product = countProduct(number[i:i+adjacentDigits])
+        if product > max {
+            max = product
+		fmt.Println(max)
+		fmt.Println(number[i:i+adjacentDigits])
+		fmt.Println()
+        }
+    }
 
     fmt.Println(max)
 }
 
-func findGreatestProductInRow(number []string, adjacentDigits int) int {
-    var count int
-    count = len(number)
-    
-    var length int
-    length = len(number[0]) - adjacentDigits
-    
-    var max int
-    var product int
-    
-    for row := 0; row < count; row++ {
-        for i := 0; i < length; i++ {
-            product = countProduct(number[row][i:i+adjacentDigits])
-            if product > max {
-                max = product
-            }
-        }
-    }
-    
-    return max
-}
-
-func findGreatestProductInCol(number []string, adjacentDigits int) int {
-    var count int
-    count = len(number)
-    
-    var length int
-    length = len(number[0]) - adjacentDigits
-    
-    var col int
-    var row int
-    var max int
-    var product int
-    
-    var productNumber string
-    
-    for col = 0; col < length; col++ {
-        for row = 0; row < count - adjacentDigits; row++ {
-            productNumber =""
-            for i := 0; i < adjacentDigits; i++ {
-                productNumber += number[i][col:col+1]
-            }
-            product = countProduct(productNumber)
-            if product > max {
-                max = product
-            }
-        }
-    }
-    
-    return max
-}
-
-func countProduct(number string) int {
-    var result int
+func countProduct(number string) int64 {
+    var result int64
     result = 1
     for i := 0; i < len(number); i++ {
 	num, _ := strconv.Atoi(number[i:i+1])
-        result *= num
+        result *= int64(num)
     }
     return result
 }
