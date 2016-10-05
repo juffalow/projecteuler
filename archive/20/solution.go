f58a471d52554e5e82d1c2c0ea5451cfbfcb9d60
Matej Jellus <juffalow@juffalow.com>
2016-10-05 10:43:43 +0200

    problem 20, little bit shorter

--- archive/20/solution.go
+++ archive/20/solution.go
@@ -25,9 +25,7 @@ func main() {
 	number = []int{1,0,0}
 
     for j := 99; j >= 2; j-- {
-		factor := numberToArray(j)
-
-		number = multiply(number, factor)
+		number = multiply(number, j)
     }
 
 	var sum int
@@ -47,83 +45,28 @@ func numberToArray(number int) []int {
 	for ; number > 0; {
 		remainder := number % 10
 
-		result = append(result, remainder)
+		result = append([]int{remainder}, result...)
 
 		number = number / 10
-	}
-
-	for i := len(result) / 2 - 1; i >= 0; i-- {
-		opp := len(result) - 1 - i
-		result[i], result[opp] = result[opp], result[i]
 	}
 
 	return result
 }
 
-func multiply(number []int, factor []int) []int {
-	var result [][]int
-	var zeros []int
-
-	for i := len(factor) - 1; i >= 0; i-- {
-		result = append(result, multiplyBy(number, factor[i], zeros))
-		zeros = append(zeros, 0)
-	}
-
-	return sum(result)
-}
-
-func multiplyBy(number []int, factor int, zeros []int) []int {
+func multiply(number []int, factor int) []int {
 	var result []int
 	var remainder int
+
 	remainder = 0
 
-	result = append(result, zeros...)
-
 	for i := len(number) - 1; i >= 0; i-- {
-		result = append(result, ((number[i] * factor) + remainder) % 10)
-		remainder = ((number[i] * factor) + remainder) / 10
-	}
-	if( remainder > 0 ) {
-		result = append(result, remainder)
-	}
-
-	return revertArray(result)
-}
-
-func revertArray(arr []int) []int {
-	for i := len(arr) / 2 - 1; i >= 0; i-- {
-		opp := len(arr) - 1 - i
-		arr[i], arr[opp] = arr[opp], arr[i]
-	}
-	return arr
-}
-
-func sum(numbers [][]int) []int {
-	var result []int
-	var index int
-	var sum int
-	var remainder int
-	var exists bool
-
-	for index = 0; ; index++ {
-		for i := 0; i < len(numbers); i++ {
-			if len(numbers[i]) - index - 1 >= 0 {
-				exists = true
-				sum += numbers[i][len(numbers[i])-index-1]
-			}
-		}
-		if !exists {
-			break
-		}
-		result = append(result, (sum + remainder) % 10)
-		remainder = (sum + remainder) / 10
-		sum = 0
-		exists = false
+		result = append([]int{((number[i] * factor) + remainder) % 10}, result...)
+		remainder =  ((number[i] * factor) + remainder) / 10
 	}
 
 	if( remainder > 0 ) {
-		result = append(result, remainder)
+		result = append(numberToArray(remainder), result...)
 	}
 
-	return revertArray(result)
+	return result
 }
